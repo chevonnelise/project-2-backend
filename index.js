713575764e8a135cd0504742b0864dc79c538352
chevonnelise <elise.chevonne@gmail.com>
2024-04-25 02:00:27 +0000

    initial setup

---
+++ index.js
@@ -0,0 +1,37 @@
+// setup express
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+require('dotenv').config();
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+// use hbs for view engine
+app.set('view engine', 'hbs');
+
+// enable static folder
+app.use(express.static('public'));
+
+// enable wax-on for template inheritance
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
+
+// enable forms
+app.use(
+    express.urlencoded({
+        'extended': false
+    })
+)
+
+async function main() {
+    // routes here
+
+}
+
+main();
+
+
+app.listen(PORT, ()=>{
+    console.log("server started on port ${PORT}")
+});
